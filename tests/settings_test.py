415129267242a46eacd436b8d90e7f8a43c55724
Juan Madurga <jlmadurga@gmail.com>
2016-01-29 19:01:32 +0100

    remove settings local

--- tests/settings_test.py
+++ tests/settings_test.py
@@ -1,4 +1,13 @@
-from demo_telegrambot.settings_local import *
+from demo_telegrambot.settings import *
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 class DisableMigrations(object):
 
